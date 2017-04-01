774c1a98a289344cdefbef99d43d3141dbb2303a
Barbara Perlik <barbperlik@gmail.com>
2017-04-01 18:41:40 -0400

    added import filename, delete, edit, and search submenu

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -76,13 +76,22 @@ class MenuController
   end
 
   def search_entries
-  # iterate thru all entries using each
-  address_book.entries.each do |entry|
+    # get the name that user want to search for and store in var name
+    print "Search by name: "
+    name = gets.chomp
+    # call search on address_book, return match or nil
+    # it will never return an empty string bec import_from_csv will fail if no name entry
+    match  = address_book.binary_search(name)
     system "clear"
-    puts entry.to_s
-  # call entry_submenu to display a submenu for each entry
-  # add this method at bottom of MenuController
-    entry_submenu(entry)
+    # check if search returned a match, call a helper method, search_submenu
+    # search_submenu displays list of operations for an entry: delete, edit, return to main menu
+    # if no match, returns nil/false
+    if match
+      puts match.to_s
+      search_submenu(match)
+    else
+      puts "No match found for #{name}"
+    end
   end
 
     system "clear"
@@ -90,6 +99,28 @@ class MenuController
   end
 
   def read_csv
+    # prompt user to enter the name of a csv file to import
+    # use chomp to removes newlines and trailing spaces
+    print "Enter CSV file to import: "
+    file_name = gets.chomp
+
+    # check to see if file name is empty and return to main if true
+    if file_name.empty?
+      system "clear"
+      puts "No CSV file read"
+      main_menu
+    end
+
+    # import the file, clear the screen, print number of entries read
+    # wrap all commands in begin/rescue block. begin protects crashing
+    begin
+      entry_count = address_book.import_from_csv(file_name).count
+      system "clear"
+      puts "#{entry_count} new entries added from #{file_name}"
+    rescue
+      puts "#{file_name} is not a valid CSV file, please enter the name of a valid CSV file"
+      read_csv
+    end
   end
 
   def entry_submenu(entry)
@@ -104,7 +135,12 @@ class MenuController
 
       when "n"
       when "d"
+        # call delete_entry, after deleted: return control to view_all_entries
+        delete_entry(entry)
       when "e"
+        # call edit_entry, then display sub-menu with entry_submenu
+        edit_entry(entry)
+        entry_submenu(entry)
       when "m"
         system "clear"
         main_menu
@@ -113,6 +149,63 @@ class MenuController
         puts "#{selection} is not a valid input"
         entry_submenu(entry)
      end
+  end
+
+  def edit_entry(entry)
+    # Print ask for each data item, assign gets.chomp to a var for each item
+    print "Updated name: "
+    name = gets.chomp
+    print "Updated phone number: "
+    phone_number = gets.chomp
+    print "Updated email: "
+    email = gets.chomp
+
+    # Use !attribute.empty? to set attributes only if a valid attrib is read
+    entry.name = name if !name.empty?
+    entry.phone_number = phone_number if !phone_number.empty?
+    entry.email = email if !email.empty?
+    system "clear"
+
+    # print out entry with updated attributes
+    puts "Updated entry: "
+    puts entry
+  end
+
+  def delete_entry(entry)
+    address_book.entries.delete(entry)
+    puts "#{entry.name} has been deleted"
+  end
+
+  def search_submenu(entry)
+    # print out the submenu
+    puts "\nd - delete entry"
+    puts "e - edit this entry"
+    puts "m - return to main menu"
+
+    # save the user input to selection
+    selection = gets.chomp
+
+    # case statement takes a specific action based on user input
+    # d = call delete_entry;  e = call edit_entry;  m = return to main menu
+    # input is no match = else clear screen and repeat search_submenu
+    case selection
+      when "d"
+        system "clear"
+        delete_entry(entry)
+        main_menu
+      when "e"
+        edit_entry (entry)
+        system "clear"
+        main_menu
+      when "m"
+        system "clear"
+        main_menu
+      else
+        system "clear"
+        puts "#{selection} is not a valid input"
+        puts entry.to_s
+        search_submenu(entry)
+      end
   end
 end
 
