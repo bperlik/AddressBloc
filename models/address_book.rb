e28dcdf5a239287a8d99554af05c0d7941fc7d9c
Barbara Perlik <barbperlik@gmail.com>
2017-03-31 20:31:29 -0400

    added binary searching

--- models/address_book.rb
+++ models/address_book.rb
@@ -34,12 +34,13 @@
      # convert each row_hash to an Entry by using add_entry method
      csv.each do |row|
        row_hash = row.to_hash
+#      p row_hash     # bloc did not have this line of code - Rizwan added during delimiter prob
        add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
      end
    end
 
    def remove_entry(name, phone_number, email)
-     #create a var to store the index that matches the desired delete
+     # create a var to store the index that matches the desired delete
      delete_index = 0
      entries.each do |remove_entry|
      # compare name with the remove_entry, if match, remove by indec
@@ -51,5 +52,36 @@
      end
      # remove matching entry at index
      entries.delete_at(delete_index)
+   end
+
+   def binary_search(name)
+     # search AddressBook for a specific entry by name
+     # use var lower to save the index of the leftmost item in array
+     lower = 0
+     upper = entries.length - 1
+
+     # use while loop while lower index less/equal upper index
+     while lower <= upper
+
+     # find middle index by sum of upper+lower / 2
+     # ruby truncates decimals, save in mid_name
+     mid = (lower + upper) / 2
+     mid_name = entries[mid].name
+
+     # compare search name to mid_name using == (case sensitive)
+     # search name =  mid name....return
+     # search name < mid name ....set upper to mid-1
+     # search name > mid name ....set lower to mid+1
+       if name == mid_name
+         return entries[mid]
+       elsif name < mid_name
+         upper = mid - 1
+       elsif name > mid_name
+         lower = mid + 1
+       end
+     end
+
+       # if no match found...return nil
+     return nil
    end
 end
